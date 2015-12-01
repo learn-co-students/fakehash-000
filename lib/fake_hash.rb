de9918877e91760b84c15292d74fd9504f2e62fa
Rigz777 <rigzgoodmusic@gmail.com>
2015-12-01 00:59:44 -0500

    done

--- lib/fake_hash.rb
+++ lib/fake_hash.rb
@@ -1,5 +1,33 @@
+require 'pry'
 class FakeHashWrapper
 
-  # write code here
+  def initialize
+    @hash = {}
+  end
 
+  def [](key)
+    @hash[key.to_sym]
+  end
+
+  def []=(key, value)
+    @hash[key.to_sym] = value
+  end
+
+  def keys
+    array = @hash.keys.sort.reverse
+  end
+
+  def first_letter(letter)
+    arr = []
+    @hash.each do |key, value|
+      if key.to_s.start_with?(letter)
+        arr << value
+      end
+    end
+    arr
+  end
+
+  def each
+    @hash.each {|k, v| yield k, v}
+  end
 end
