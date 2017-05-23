32882e6a679852e4dc41f15eaef57190b329b6f4
OnlyHughMan <hughvidler@hotmail.com>
2017-05-23 16:09:57 +1000

    Done.

--- lib/fake_hash.rb
+++ lib/fake_hash.rb
@@ -1,5 +1,35 @@
 class FakeHashWrapper
+	attr_accessor :fake_hash
 
-  # write code here
+	def initialize(*splat)
+		@fake_hash = {}
+	end
+
+	def [](key)
+		fake_hash[key.to_sym]
+	end
+
+	def []=(key, value)
+		@fake_hash[key.to_sym] = value
+	end
+
+	def keys
+		@fake_hash.keys.sort.reverse
+	end
+
+	def first_letter(l)
+		@fake_hash.each_with_object([]) do |(key, value), array|
+      	array << value if key[0] == l
+		end
+	end
+
+	def each
+		@fake_hash.each do |k, v|
+			yield k, v
+		end
+	end
+
+
+
 
 end
