b7b15f29407bb065b928bb52f245c27bda69629b
iwilliams83 <indirajamurath@gmail.com>
2017-12-02 01:45:45 +0000

    Done.

--- lib/fake_hash.rb
+++ lib/fake_hash.rb
@@ -1,5 +1,54 @@
 class FakeHashWrapper
+  require 'ostruct'
+  attr_accessor :key, :value
 
-  # write code here
+  def initialize
+    @my_hash = {}
+  end
+
+  def [](key)
+    if key.is_a? String
+      key = key.intern
+      @my_hash[key]
+    else
+      @my_hash[key]
+    end
+  end
+
+  def []=(key, value)
+    if key.is_a? String
+      key = key.intern
+      @my_hash[key] = value
+    else
+      @my_hash[key] = value
+    end
+  end
+
+  def keys
+    arr = []
+    @my_hash.each do |k,v|
+      arr << k
+    end
+    arr.sort.reverse
+  end
+
+  def first_letter(letter)
+    arr = []
+    @my_hash.each do |k,v|
+      if k.to_s.split("")[0] == letter
+        arr << v
+      end
+    end
+    return arr
+  end
+
+  def each
+    arr = @my_hash.to_a
+    i = 0
+    while i < arr.length
+      yield(arr[i])
+      i += 1
+    end
+  end
 
 end
